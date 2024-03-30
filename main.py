a0cce6b840a009b40a79114372bed446719429ea
Fankeks <fkoleda.2012@gmail.com>
2024-03-30 11:04:35 +0300

    Speed measurment

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
-from machine import Pin
-from machine import Timer
+import machine
+import time
+import random
+
+print(machine.freq())
+n = 3000
+arr1 = [random.random() for _ in range(n)]
+arr2 = [random.random() for _ in range(n)]
+
+start = time.time_ns()
+for i in range(n):
+    arr1[i] + arr2[i]
+    #print(arr3[i])
+print(time.time_ns() - start)
 
 
-p0 = Pin(2, Pin.OUT)    # create output pin on GPIO0
+'''p0 = Pin(2, Pin.OUT)    # create output pin on GPIO0
 
 
 def tim1(_):
@@ -15,4 +27,4 @@ tim0.init(period=100, mode=Timer.PERIODIC, callback=tim1)
 
 
 while 1:
-    pass
+    pass'''
